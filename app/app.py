9041663636ac19c7441066250dd877fd38219a60
Muhammad <faizanafzaal868@gmail.com>
2026-05-03 14:33:26 +0500

    Testing 11

--- app/app.py
+++ app/app.py
@@ -7,10 +7,6 @@ users = [
     {"id": 2, "name": "Bob",   "role": "developer"},
 ]
 
-# You accidentally hardcode a password
-SECRET_KEY = "mypassword123"
-db_password = "admin@123"
-
 @app.route("/")
 def home():
     return jsonify({
